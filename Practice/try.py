85c2182aee4d48b48b2b6ff1f68085f2ef85d69a
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2024-08-17 19:08:31 +0000

    Ex 10

--- Practice/try.py
+++ Practice/try.py
@@ -25,9 +25,57 @@
 # print(con)
 
 
-data = input()
-l = data.split(",")
-t = tuple(l)
+# data = input()
+# l = data.split(",")
+# t = tuple(l)
 
-print(l)
-print(t)
+# print(l)
+# print(t)
+
+
+# class _string_(object):
+#     def __init__(self):
+#         self.s = ""
+
+#     def getString(self):
+#         self.s = input()
+
+#     def printString(self):
+#         print(self.s.upper())
+
+# data = _string_()
+# data.getString()
+# data.printString()
+
+# import math
+
+# C, H = 50, 30
+# D_l = input()
+# D = D_l.split(",")
+# Q = []
+# for d in D:
+#     Q.append(str(math.sqrt((2*C*int(d)) / H)))
+
+# print(','.join(Q))
+
+# input_str = input()
+# dimensions = [int(x) for x in input_str.split(",")]
+# ans = [[0 for col in range(dimensions[1])] for row in range(dimensions[0])]
+# for row in range(dimensions[0]):
+#     for col in range(dimensions[1]):    
+#         ans[row][col] = row * col
+
+# print(ans) 
+
+# data = input()
+# data_l = data.split(",")
+# data_l.sort()
+# print(",".join(data_l))
+
+# data = input()
+# data = data.upper()
+# print(data)
+
+s = input()
+words = [word for word in s.split(" ")]
+print(" ".join(sorted(list(set(words)))))
