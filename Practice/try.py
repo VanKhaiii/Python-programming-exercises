28e7031a42e0d9a75c9eded48a7c9e410804cd0a
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2024-08-19 01:25:07 +0000

    ex 45

--- Practice/try.py
+++ Practice/try.py
@@ -195,3 +195,213 @@
 
 # for i in putNumbers(100):
 #     print(i)
+
+# import math
+
+# pos = [0,0]
+# while True:
+#     s = input()
+#     if not s:
+#         break
+#     move = s.split(" ")
+#     direc = move[0], val = int(move[1])
+#     if direc == "UP":
+#         pos[0] += val
+#     elif direc == "DOWN":
+#         pos[0] -= val 
+#     elif direc == "LEFT":
+#         pos[1] -= val
+#     elif direc == "RIGHT":
+#         pos[1] += val
+#     else: pass 
+
+# print(int(round(math.sqrt(pos[1]**2+pos[0]**2))))    
+
+# freq = {}
+# data = input()
+
+# for word in data.split(" "):
+#     freq[word] = freq.get(word, 0) + 1
+
+# words = freq.keys()
+# sorted(words)
+
+# for w in words:
+#     print("%s:%d" % (w,freq[w]))
+
+# def square(n):
+#     return n*n 
+# print(square(3))
+
+# print(abs.__doc__)
+# print(int.__doc__)
+# print(input.__doc__)
+
+# def square(num):
+#     '''Return the square value of the input number.
+    
+#     The input number must be integer.
+#     '''
+#     return num ** 2
+
+# print(square(2))
+# print(square.__doc__)
+
+# class Person:
+#     name = "Khai"
+
+#     def __init__(self, name = None):
+#         self.name = name 
+
+# jeffrey = Person("Jeffrey")
+# print("%s name is %s" % (Person.name, jeffrey.name))
+
+# nico = Person()
+# nico.name = "Nico"
+# print("%s name is %s" % (Person.name, nico.name)) 
+
+# def SumFunction(number1, number2):
+# 	return number1+number2
+
+# print(SumFunction(1,2))
+
+# def printValue(n):
+#     print(str(n))
+
+# printValue(3)
+
+# def printValue(n):
+#     print(str(n))
+
+# printValue(3)
+
+# def printValue(s1,s2):
+#     print(int(s1)+int(s2))
+
+# printValue("3","4")
+
+# def printValue(s1,s2):
+#     print(s1+s2)
+
+# printValue("3","4") #34
+
+# def printValue(s1,s2):
+#     len1 = len(s1)
+#     len2 = len(s2)
+#     if len1>len2:
+#         print(s1)
+#     elif len2>len1:
+#         print(s2)
+#     else:
+#         print(s1)
+#         print(s2)
+        
+# printValue("one","three")
+
+# def checkValue(n):
+#     if n%2 == 0:
+#         print("It is an even number")
+#     else:
+#         print("It is an odd number")
+        
+# checkValue(7)
+
+# def printDict():
+#     d=dict()
+#     d[1]=1
+#     d[2]=2**2
+#     d[3]=3**2
+#     print(d)\
+
+# def ans():
+#     d = {}
+#     for i in range(1, 21):
+#         d[i] = i**2
+#     print(d)
+
+# ans()
+
+# def ans():
+#     d = {}
+#     for i in range(1, 21):
+#         d[i] = i**2
+#     for k, v in d.items():
+#         print(v)
+
+# ans()
+
+# def printDict():
+# 	d=dict()
+# 	for i in range(1,21):
+# 		d[i]=i**2
+# 	for k in d.keys():	
+# 		print(k)
+
+# printDict()
+
+# def so():
+#     for i in range(1, 5):
+#         print("X"*i)
+
+# so()
+
+# def printList():
+# 	li=list()
+# 	for i in range(1,21):
+# 		li.append(i**2)
+# 	print(li[:5])
+
+# printList()
+
+# def printList():
+# 	li=list()
+# 	for i in range(1,21):
+# 		li.append(i**2)
+# 	print(li[-5:])
+
+# printList()
+
+# def printList():
+# 	li=list()
+# 	for i in range(1,21):
+# 		li.append(i**2)
+# 	print li[5:]
+
+# printList()
+
+# def printTuple():
+# 	li=list()
+# 	for i in range(1,21):
+# 		li.append(i**2)
+# 	print(tuple(li))
+		
+# printTuple()
+
+# tp=(1,2,3,4,5,6,7,8,9,10)
+# tp1=tp[:5]
+# tp2=tp[5:]
+# print(tp1)
+# print(tp2)
+
+# tp=(1,2,3,4,5,6,7,8,9,10)
+# li=list()
+# for i in tp:
+# 	if tp[i - 1]%2==0:
+# 		li.append(tp[i-1])
+
+# tp2=tuple(li)
+# print(tp2)
+
+# s= raw_input()
+# if s=="yes" or s=="YES" or s=="Yes":
+#     print "Yes"
+# else:
+#     print "No"
+
+li = [1,2,3,4,5,6,7,8,9,10]
+evenNumbers = filter(lambda x: x%2==0, li)
+print(list(evenNumbers))
+
+
+
+
